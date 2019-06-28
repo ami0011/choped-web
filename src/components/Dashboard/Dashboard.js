b4e57780b74c234717116b5e4f47118120278c82
Monalisa93 <monalisa93.debnath@gmail.com>
2019-06-28 19:39:49 +0800

    Chats

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import UserProfile from "./UserProfile/UserProfile";
 import Conversations from "./Conversations/Conversations";
 import Groups from "./Groups/Groups";
 import Files from "./Files/Files";
+import Chat from "./Chat/Chat";
 
 const { Content, Sider } = Layout;
 
@@ -13,8 +14,8 @@ const tabs = [
   { key: 1, title: "Profile", icon: "user", component: <UserProfile/> },
   { key: 2, title: "Conversation", icon: "message", component: <Conversations/> },
   { key: 3, title: "Groups", icon: "team", component: <Groups/> },
-    { key: 4, title: "Chat", icon: "wechat", component: <Groups/> },
-    { key: 5, title: "Files", icon: "file", component: <Files/> }
+    { key: 4, title: "Chat", icon: "wechat", component: <Chat /> },
+    { key: 5, title: "Files", icon: "file", component: <Files /> }
 ];
 
 export class Dashboard extends Component {
