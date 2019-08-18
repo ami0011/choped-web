2d3a03e16c9e55d37afcd4318ee7698eefd5f7ed
Monalisa93 <monalisa93.debnath@gmail.com>
2019-08-18 23:52:09 +0800

    file upload

--- src/components/Dashboard/Files/Files.js
+++ src/components/Dashboard/Files/Files.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Table from "../../UI/Table/Table";
 import axios from "../../../axiosInstance";
 import { connect } from "react-redux";
+import { Upload, Button, Icon } from 'antd';
+
+const props = {
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange({ file, fileList }) {
+        if (file.status !== 'uploading') {
+            console.log(file, fileList);
+        }
+    },
+    defaultFileList: [
+        {
+            uid: '1',
+            name: 'sample1.png',
+            status: 'done',
+            response: 'Server Error 500', // custom error message to show
+            url: 'http://www.baidu.com/xxx.png',
+        },
+        {
+            uid: '2',
+            name: 'sample2.png',
+            status: 'done',
+            url: 'http://www.baidu.com/yyy.png',
+        },
+        {
+            uid: '3',
+            name: 'sample3.png',
+            status: 'error',
+            response: 'Server Error 500', // custom error message to show
+            url: 'http://www.baidu.com/zzz.png',
+        },
+    ],
+};
 
 class Files extends Component {
     state={
-      files: []
+        files: [],
+        selectedFile: null
     };
 
     componentWillMount() {
@@ -52,6 +85,13 @@ class Files extends Component {
         return (
             <div>
                 <Table columns={columns} dataSource={this.state.files}/>
+                <div style={{ marginTop: '30px', maxWidth: '150px', marginRight: 'auto', marginLeft: 'auto' }}>
+                <Upload {...props}>
+                    <Button>
+                        <Icon type="upload" /> Click to Upload
+                    </Button>
+                </Upload>
+                </div>
             </div>
         );
     }
