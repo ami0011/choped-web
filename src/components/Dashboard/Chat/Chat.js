48b2759b39f52d9392d8ac25fb2f3db507171ee0
Monalisa93 <monalisa93.debnath@gmail.com>
2019-08-26 19:04:19 +0800

    video chat

--- src/components/Dashboard/Chat/Chat.js
+++ src/components/Dashboard/Chat/Chat.js
@@ -8,9 +8,11 @@ class Chat extends Component {
     return (
       <div>
         <div id="frame">
-          <div style={{ textAlign: "center", height: "100vh" }}>
-            <iframe src="https://phasic-chatsvr.herokuapp.com" width="100%" height="100%" style={{ height: "-webkit-fill-available"}} />
+          <div>
+            <iframe src="https://tokbox.com/embed/embed/ot-embed.js?embedId=245fdf36-e100-496f-8e9f-4a67b086f2e9&room=DEFAULT_ROOM&iframe=true" width="100%" height="400px" scrolling="auto" allow="microphone; camera" />
+            <iframe src="https://phasic-chatsvr.herokuapp.com" width="100%" height="400px" />
           </div>
+
         </div>
       </div>
     );
---
+++ src/config.js
@@ -0,0 +1,7 @@
+// tokbox
+
+export default {
+    API_KEY: '3685382',
+    SESSION_ID: '',
+    TOKEN: 'dd396b326ee1edc605d565a6afcdbfb05eade00c'
+};
